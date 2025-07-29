bcf09185bb4942a81bc747de9c3ec31c043f1f4a
kevin-acuna <kjacuna.inf@gmail.com>
2025-07-29 02:58:49 +0200

    calibrcion y main

--- MATLAB_exp_estimation3D/main.m
+++ MATLAB_exp_estimation3D/main.m
@@ -2,7 +2,7 @@
 
 % Configuración
 clear;
-close all;
+% close all;
 clc;
 
 % Constantes
@@ -12,8 +12,8 @@ m = 3; % Orden Lambertiano (ajustar según sea necesario)
 % csv_file = '../dataset/data_0.000000_0.000000_1.000000.csv';
 % csv_file = '../dataset/data_-0.600000_0.600000_1.200000.csv';
 % csv_file = '../dataset/data_-0.600000_0.000000_1.200000.csv';
-% csv_file = '../dataset/calibration/data_-0.600000_0.400000_0.800000.csv';
-csv_file = '../dataset/calibration/data_0.000000_0.000000_0.800000.csv';
+csv_file = '../dataset/sabado26/data_-0.600000_0.400000_0.800000.csv';
+% csv_file = '../dataset/calibration/data_0.000000_0.000000_0.800000.csv';
 
 
 % ======================================================================
