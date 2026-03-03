99f42f36596cdfdb100158a2123469b03f598bb8
kevin-acuna <kjacuna.inf@gmail.com>
2026-03-03 15:39:28 +0100

    data en 3D

--- Test1/src/server/config.py
+++ Test1/src/server/config.py
@@ -41,8 +41,8 @@ THETA_MAX = 15  # 0 = vertical, max inclination from vertical
 
 WS_R_INNER = 0.65 # Default inner radius of the workspace
 WS_R_OUTER = 0.85 # Default outer radius of the workspace
-WS_THETA_INIT = 100 # Initial angle of the workspace:190
-WS_THETA_FIN = 270 # Final angle of the workspace
+WS_THETA_INIT = 180 # Initial angle of the workspace:190
+WS_THETA_FIN = 355 # Final angle of the workspace
 
 # Height-dependent workspace radii
 WS_HEIGHT_RADII = {
