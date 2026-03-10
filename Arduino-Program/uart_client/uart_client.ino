ecd874c9202620e6d638cc79c3d10e00b93004b3
kevin-acuna <kjacuna.inf@gmail.com>
2026-03-10 15:51:44 +0100

    arduino: add laser pin

--- Arduino-Program/uart_client/uart_client.ino
+++ Arduino-Program/uart_client/uart_client.ino
@@ -6,25 +6,29 @@
 */
 
 const int pinLed = 6; // pin D6
+const int pinLaser = 4; // activate the laser pin
+
 void setup() {
   // Inicializar puerto serial a 115200 (ajusta igual que en PC)
   Serial.begin(115200);
   
   // Configurar pin D6 y LED_BUILTIN como salida
   pinMode(pinLed, OUTPUT);
+  pinMode(pinLaser, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   
   // Apagar ambos de inicio
   digitalWrite(pinLed, LOW);
+  digitalWrite(pinLaser, LOW);
   digitalWrite(LED_BUILTIN, LOW);
 
   for(int i=0; i<3; i++){
     digitalWrite(LED_BUILTIN, HIGH); 
     digitalWrite(pinLed, HIGH);
-    delay(1000);
+    delay(500);
     digitalWrite(pinLed, LOW);
     digitalWrite(LED_BUILTIN, LOW); 
-    delay(1000);
+    delay(500);
   }
 }
 
